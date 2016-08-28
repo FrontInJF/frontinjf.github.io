3809bad6e928208eb938c5cccb2650b4fe5b1141
Lesley Andrez <lesley.andrez@gmail.com>
2016-08-28 16:39:39 -0300

    debug travis

--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -3,9 +3,9 @@ rm -rf out || exit 0;
 echo "Fazendo o build..."
 ( cd src/site
   git init
-  git config --global user.email "nobody@nobody.org"
-  git config --global user.name "Travis CI"
+  git config --global user.email "lesley.andrez@gmail.com"
+  git config --global user.name "lesleyandrez"
   git add .
   git commit -m "Deployed to Github Pages"
-  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
 )
