9f3dc0985d1a905184af41bd4152faf91221cedd
Lesley Andrez <lesley.andrez@gmail.com>
2016-08-28 16:35:05 -0300

    debug travis

--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
-# See https://medium.com/@nthgergo/publishing-gh-pages-with-travis-ci-53a8270e87db
-set -o errexit
-
-rm -rf public
-mkdir public
-
-# config
-git config --global user.email "nobody@nobody.org"
-git config --global user.name "Travis CI"
-
-# build (CHANGE THIS)
-echo "fazendo o build do site e do blog..."
-
-# debug
-pwd
-ls -al
-# deploy
-cd src/site
-git init
-git add .
-git commit -m "Deploy to Github Pages"
-git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
+rm -rf out || exit 0;
+echo "Fazendo o build..."
+( cd src/site
+  git init
+  git config --global user.email "nobody@nobody.org"
+  git config --global user.name "Travis CI"
+  git add .
+  git commit -m "Deployed to Github Pages"
+  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+)
